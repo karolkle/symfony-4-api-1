a96ee1bb117481b8fb0c2aee4fee9ea9e5fbcded
KarolKlech <42314794+karolkle@users.noreply.github.com>
2019-10-15 18:59:55 +0200

    Update AllCompetitionTest.php

--- tests/Controller/AllCompetitionTest.php
+++ tests/Controller/AllCompetitionTest.php
@@ -1,13 +1,1 @@
-<?php
 
-
-namespace App\Tests\Controller;
-
-use App\Controller\CompetitionController;
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class AllCompetitionTest extends WebTestCase
-{
-    public function testCompetitionControllerisTrue(){
-    $request = $this->getMock("Symfony\Component\HttpFoundation\JsonResponse");
-}}
